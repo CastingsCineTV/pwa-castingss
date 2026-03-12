ed304949bf4ed3bbfbfd5e7b2f86e95b9e146709
CastingsCineTV <castingscinetv@gmail.com>
2026-03-12 19:23:16 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,3 +4,30 @@ self.addEventListener('install', function(e) {
 
 self.addEventListener('fetch', function(event) {
 });
+const CACHE_NAME = "castings-app-v1";
+
+const urlsToCache = [
+"/",
+"https://www.castingscinetv.com/",
+"https://www.castingscinetv.com/search/label/M%C3%A9xico",
+"https://www.castingscinetv.com/search/label/Argentina",
+"https://www.castingscinetv.com/search/label/España"
+];
+
+self.addEventListener("install", event => {
+event.waitUntil(
+caches.open(CACHE_NAME)
+.then(cache => {
+return cache.addAll(urlsToCache);
+})
+);
+});
+
+self.addEventListener("fetch", event => {
+event.respondWith(
+caches.match(event.request)
+.then(response => {
+return response || fetch(event.request);
+})
+);
+});
